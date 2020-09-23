736a428a46ca8847e68c0dd5098f33ab813b5c26
kibartas <juodasis.perlas@gmail.com>
2020-09-23 20:19:45 +0300

    fix the problem with signal strength; also fix the problem with the SSID being too long. If the SSID was too long DWMBLOCKS would crash DWM and I'd need to edit the network_status.sh script from another tty with root. Now it's fixed and seems stable

--- network_status.sh
+++ network_status.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
 
+# DWMBLOCKS config information
+DWMBLOCKS_BLOCK_SIZE=80
+DWMBLOCKS_DELIMITER=5
+DWMBLOCKS_BLOCK_SIZE=$(($DWMBLOCKS_BLOCK_SIZE-$DWMBLOCKS_DELIMITER))
+
 DEFAULT_COLOR='^c#FFFFFF^'
 RED='^c#FF0000^'
 GREEN='^c#00FF00^'
 
 #Prep
-SSID=$(iw wlp3s0 info | grep -e 'ssid' | cut -sf2- -d\ )
+#SSID=$(iw wlp3s0 info | grep -e 'ssid' | cut -sf2- -d\ )
 IP=$(ip route get 1 | awk '{print $7;exit}')
+SSID="kenhamthescienceguy"
 
-#Getting problems running this. Seems to be something with color. Dwm crashes if I run the script with this in SSID_IP variable
-#SIGNAL_STRENGTH=$(awk 'NR==3 {print int($3 / 10)}' /proc/net/wireless)
+# Fixed the problem
+SIGNAL_STRENGTH=0.$(awk 'NR==3 {print int($3 / 10)}' /proc/net/wireless)
 
 if test -z $IP; then 
     SSID_IP="$RED No connection $DEFAULT_COLOR" 
 else 
-    SSID_IP="$GREEN $IP @ $SSID $DEFAULT_COLOR"
+    SSID_IP="$GREEN($SIGNAL_STRENGTH) $IP @ $SSID$DEFAULT_COLOR"
+    LEN=$(echo $SSID_IP | wc -m)
+    if test $LEN -ge $DWMBLOCKS_BLOCK_SIZE; then
+        HOW_MUCH_TRUNC=$(($LEN-$DWMBLOCKS_BLOCK_SIZE+3))
+        SSID_LEN=$(echo $SSID | wc -m)
+        TRUNC_TO=$(($SSID_LEN-$HOW_MUCH_TRUNC))
+        SSID=$(echo $SSID | cut -c1-$TRUNC_TO)
+        SSID=$(echo $SSID"...")
+        SSID_IP="$GREEN($SIGNAL_STRENGTH) $IP @ $SSID$DEFAULT_COLOR"
+    fi
+
 fi
-#if test -z $IP; then SSID_IP="No connection"; else SSID_IP="($SIGNAL_STRENGTH) $IP @ $SSID"; fi
 
 #Output
-echo "$SSID_IP"
+echo $SSID_IP
