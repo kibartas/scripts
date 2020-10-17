31898ee363dc3653849b04dd4252a8cba947ec79
kibartas <juodasis.perlas@gmail.com>
2020-10-17 20:05:25 +0300

    create a simple script that logs the current timer information to a file

---
+++ timer_log.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SCRIPTS=/home/kibartas/scripts
+
+printf "%s %s" $(cat $SCRIPTS/timer_name.txt) $(cat $SCRIPTS/timer.txt) >> $SCRIPTS/timer.log
+
+echo 00:00:00 > $SCRIPTS/timer.txt
+
+pkill -RTMIN+3 dwmblocks
