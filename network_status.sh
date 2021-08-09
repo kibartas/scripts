709ce9a13ecbe9d89a8f7b0c97133bf292c9c38e
kibartas <juodasis.perlas@gmail.com>
2021-08-09 14:37:16 +0300

    remove interface names from the status bar for space savings (it's already apparent when the wireless if is on - when there is a SSID)

--- network_status.sh
+++ network_status.sh
@@ -9,28 +9,31 @@ DEFAULT_COLOR='^c#FFFFFF^'
 RED='^c#FF0000^'
 GREEN='^c#00FF00^'
 
-#Prep
-SSID=$(iw wlp3s0 info | grep -e 'ssid' | cut -sf2- -d\ )
-IP=$(ip route get 1 | awk '{print $7;exit}')
 
-# Fixed the problem
-SIGNAL_STRENGTH=0.$(awk 'NR==3 {print int($3 / 10)}' /proc/net/wireless)
-
-if test -z $IP; then 
-    SSID_IP="$RED No connection $DEFAULT_COLOR" 
-else 
-    SSID_IP="$GREEN($SIGNAL_STRENGTH) $IP @ $SSID$DEFAULT_COLOR"
-    LEN=$(echo $SSID_IP | wc -m)
-    if test $LEN -ge $DWMBLOCKS_BLOCK_SIZE; then
-        HOW_MUCH_TRUNC=$(($LEN-$DWMBLOCKS_BLOCK_SIZE+3))
-        SSID_LEN=$(echo $SSID | wc -m)
-        TRUNC_TO=$(($SSID_LEN-$HOW_MUCH_TRUNC))
-        SSID=$(echo $SSID | cut -c1-$TRUNC_TO)
-        SSID=$(echo $SSID"...")
-        SSID_IP="$GREEN($SIGNAL_STRENGTH) $IP @ $SSID$DEFAULT_COLOR"
+ETH=$(ip route | grep default | grep enp2s0f1 | awk '{print $9}')
+if test $ETH; then # Ethernet
+    IP="$GREEN $ETH "
+    echo "$IP$DEFAULT_COLOR"
+else #Wireless
+    SSID=$(iw wlp3s0 info | grep -e 'ssid' | cut -sf2- -d\ )
+    IP="$(ip route | grep default | grep wlp3s0 | awk '{print $9}')"
+    if test -z "$IP"; then 
+        SSID_IP="$RED No connection $DEFAULT_COLOR" 
+    else 
+        SSID_IP="$GREEN $IP @ $SSID$DEFAULT_COLOR"
+        LEN=$(echo $SSID_IP | wc -m)
+        if test $LEN -ge $DWMBLOCKS_BLOCK_SIZE; then
+            HOW_MUCH_TRUNC=$(($LEN-$DWMBLOCKS_BLOCK_SIZE+3))
+            SSID_LEN=$(echo $SSID | wc -m)
+            TRUNC_TO=$(($SSID_LEN-$HOW_MUCH_TRUNC))
+            SSID=$(echo $SSID | cut -c1-$TRUNC_TO)
+            SSID=$(echo $SSID"...")
+            SSID_IP="$IP @ $SSID$DEFAULT_COLOR"
+        fi
     fi
-
+    echo $SSID_IP
 fi
+
 
 #Output
 echo $SSID_IP
